647b8e0b50459d698fe29a3fc4358e9c88605cd3
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-08 16:05:53 +0700

    added multi color gradient behind robot graphic

--- components/Hero.js
+++ components/Hero.js
@@ -61,6 +61,11 @@ const Hero = () => {
           alt="billing"
           className="relative z-[5] h-[100%] w-[100%]"
         />
+        {/* gradient start */}
+        <div className="pink__gradient absolute top-0 z-[0] h-[35%] w-[40%]" />
+        <div className="white__gradient absolute bottom-40 z-[1] h-[80%] w-[80%] rounded-full" />
+        <div className="blue__gradient absolute bottom-20 right-20 z-[0] h-[50%] w-[50%]" />
+        {/* gradient end */}
       </div>
     </section>
   );
