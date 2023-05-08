4686c54b9bc59929ac68d0366d994c4d05b58681
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-08 13:36:11 +0700

    add page scaffhold structure

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { Navbar } from '@/components';
 
 export default function Home() {
   return (
-    <div>Hello world!</div>
-  )
+    <div className="w-full overflow-hidden bg-primary">
+      <div className="flex items-center justify-center px-6 sm:px-16">
+        <div className="w-full xl:max-w-[1280px]">
+          <Navbar />
+        </div>
+      </div>
+
+      <div className="flex items-start justify-center bg-primary">
+        <div className="w-full xl:max-w-[1280px]">Hero</div>
+      </div>
+
+      <div className="flex items-center justify-center bg-primary px-6 sm:px-16">
+        <div className="w-full xl:max-w-[1280px]">
+          Stats Business Billing CardDeal Testimonials Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
 }
