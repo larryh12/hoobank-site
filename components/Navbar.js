b1390649af809da567878c6b91cf8b84ab5e3993
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-08 14:26:27 +0700

    add navbar starter layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,46 @@
 import React from 'react';
 
+import Image from 'next/image';
+import close from '@/public/close.svg';
+import logo from '@/public/logo.svg';
+import menu from '@/public/menu.svg';
+
+const navLinks = [
+  {
+    id: 'home',
+    title: 'Home',
+  },
+  {
+    id: 'features',
+    title: 'Features',
+  },
+  {
+    id: 'product',
+    title: 'Product',
+  },
+  {
+    id: 'clients',
+    title: 'Clients',
+  },
+];
+
 const Navbar = () => {
-  return <div className="text-white">Navbar</div>;
+  return (
+    <nav className="flex w-full items-center justify-between py-6">
+      <Image src={logo} alt="hoobank" className="h-[32px] w-[124px]" />
+
+      <ul className="hidden flex-1 list-none items-center justify-end sm:flex">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className="mr-10 cursor-pointer font-sans text-[16px] font-normal text-white"
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
