f5e5d90828a6729a72ca3347da1bfaa8bf046c04
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-09 14:46:59 +0700

    complete business left section

--- components/Business.js
+++ components/Business.js
@@ -1,7 +1,29 @@
 import React from 'react';
 
 const Business = () => {
-  return <div className="text-white">Business</div>;
+  return (
+    <section id="features" className="flex flex-col py-6 sm:py-16 md:flex-row">
+      {/* left section */}
+      <div className="flex flex-1 flex-col items-start justify-center">
+        <h2 className="w-full font-sans text-[40px] font-semibold leading-[66.8px] text-white xs:text-[48px] xs:leading-[76.8px]">
+          You do the business, <br className="hidden sm:block" /> we’ll handle
+          the money.
+        </h2>
+        <p className="mt-5 max-w-[470px] font-sans text-[18px] font-normal leading-[30.8px] text-dimWhite">
+          With the right credit card, you can improve your financial life by
+          building credit, earning rewards and saving money. But with hundreds
+          of credit cards on the market.
+        </p>
+
+        <button
+          type="button"
+          className="bg-blue-gradient mt-10 rounded-[10px] px-6 py-4 font-sans text-[18px] font-medium text-primary outline-none"
+        >
+          Get Started
+        </button>
+      </div>
+    </section>
+  );
 };
 
 export default Business;
