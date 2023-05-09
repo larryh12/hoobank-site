f7554b454b0cde8b6951a270a0b6b53298f8471e
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-09 14:35:01 +0700

    complete stats section

--- components/Stats.js
+++ components/Stats.js
@@ -19,7 +19,23 @@ const stats = [
 ];
 
 const Stats = () => {
-  return <div className="text-white">Stats</div>;
+  return (
+    <section className="mb-6 flex flex-row flex-wrap items-center justify-center sm:mb-20">
+      {stats.map((stat) => (
+        <div
+          key={stat.id}
+          className={`m-3 flex flex-1 flex-row items-center justify-start`}
+        >
+          <h4 className="font-sans text-[30.89px] font-semibold leading-[43.16px] text-white xs:text-[40.89px] xs:leading-[53.16px]">
+            {stat.value}
+          </h4>
+          <p className="text-gradient ml-3 font-sans text-[15.45px] font-normal uppercase leading-[21.58px] xs:text-[20.45px] xs:leading-[26.58px]">
+            {stat.title}
+          </p>
+        </div>
+      ))}
+    </section>
+  );
 };
 
 export default Stats;
