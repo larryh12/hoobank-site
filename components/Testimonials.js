f66bd1f48abeae27b19ac5ab9bed692256f3529d
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-09 16:15:01 +0700

    add testimonials section content

--- components/Testimonials.js
+++ components/Testimonials.js
@@ -32,7 +32,32 @@ const feedback = [
 ];
 
 const Testimonials = () => {
-  return <div className="text-white">Testimonials</div>;
+  return (
+    <section
+      id="clients"
+      className="relative flex flex-col items-center justify-center py-6 sm:py-16"
+    >
+      <div className="blue__gradient absolute -right-[50%] bottom-40 z-[0] h-[60%] w-[60%] rounded-full" />
+
+      <div className="relative z-[1] mb-6 flex w-full flex-col items-center justify-between sm:mb-16 md:flex-row">
+        <h2 className="w-full font-sans text-[40px] font-semibold leading-[66.8px] text-white xs:text-[48px] xs:leading-[76.8px]">
+          What People are <br className="hidden sm:block" /> saying about us
+        </h2>
+        <div className="mt-6 w-full md:mt-0">
+          <p className="max-w-[450px] text-left font-sans text-[18px] font-normal leading-[30.8px] text-dimWhite">
+            Everything you need to accept card payments and grow your business
+            anywhere on the planet.
+          </p>
+        </div>
+      </div>
+
+      <div className="feedback-container relative z-[1] flex w-full flex-wrap justify-center sm:justify-start">
+        {feedback.map((card) => (
+          <FeedbackCard key={card.id} {...card} />
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
