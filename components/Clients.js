549398f831ad7ac39ad871ad90c19a2d19b7ab78
Larry H <111525408+larryh12@users.noreply.github.com>
2023-05-09 20:51:30 +0700

    static and const import

--- components/Clients.js
+++ components/Clients.js
@@ -1,4 +1,28 @@
 import React from 'react';
+import Image from 'next/image';
+import airbnb from '@/public/airbnb.png';
+import binance from '@/public/binance.png';
+import coinbase from '@/public/coinbase.png';
+import dropbox from '@/public/dropbox.png';
+
+const clients = [
+  {
+    id: 'client-1',
+    logo: airbnb,
+  },
+  {
+    id: 'client-2',
+    logo: binance,
+  },
+  {
+    id: 'client-3',
+    logo: coinbase,
+  },
+  {
+    id: 'client-4',
+    logo: dropbox,
+  },
+];
 
 const Clients = () => {
   return <div className="text-white">Clients</div>;
